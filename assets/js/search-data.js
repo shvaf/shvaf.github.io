175573d0a6ca202351d6042a1c85fd54463430f0
shvaf <67230005+shvaf@users.noreply.github.com>
2025-05-27 03:33:32 +0000

    Deploying to gh-pages from @ shvaf/shvaf.github.io@31fb8b82523dc580f51600a69201c55132de3a7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-determination-of-deep-vein-thrombosis-diagnosis-using-python-and-natural-language-processing",
+          title: 'Determination of Deep Vein Thrombosis Diagnosis using Python and Natural Language Processing',
+          description: "Published May 26, 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
