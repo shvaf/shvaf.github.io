8ec83f19acd0e79ad55c40beb9ecbf7548ce05bf
shvaf <67230005+shvaf@users.noreply.github.com>
2025-08-28 20:19:27 +0000

    Deploying to gh-pages from @ shvaf/shvaf.github.io@2c1a836e16e469f747995b2d3a38ac4c2dd992ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,9 +466,9 @@ ninja.data = [{
           description: "published June, 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
